45cfc1f33dec17d933c837c19e5002528cec9803
unknown <kanikayadav250@gmail.com>
2021-01-25 23:47:12 +0530

    first commit

--- Weather/app/src/main/java/com/example/weather/MainActivity.java
+++ Weather/app/src/main/java/com/example/weather/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.weather;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,48 +11,85 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HttpHeaderParser;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.UnsupportedEncodingException;
+
+import static android.app.Notification.EXTRA_TEXT;
+
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_TEXT="com.example.weather.example.EXTRA_TEXT";
+    public static final String EXTRA_TEXT1="com.example.weather.example.EXTRA_TEXT1";
+    public static final String EXTRA_NUMBER="com.example.weather.example.EXTRA_NUMBER";
+    public static final String EXTRA_NUMBER1="com.example.weather.example.EXTRA_NUMBER1";
+
+
 
     EditText cn;
-    //TextView tem;
+    Button btn;
+    String temper;
+  public static   String city;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
         cn = (EditText) findViewById(R.id.et);
-        //tem = (TextView) findViewById(R.id.tv1);
+        btn =(Button) findViewById(R.id.bt);
+        btn.setBackgroundColor(Color.BLACK);
+
     }
 
+         public void getWeather (View view){
 
-    public void getWeather (View view){
-        String apikey = "b39de1a4246f749bacef1271268b429a";
-        String city = cn.getText().toString();
-        String url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=b39de1a4246f749bacef1271268b429a";
+
+             city = cn.getText().toString();
+
+              final String API_KEY = BuildConfig.ApiKey;
+              final String url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=b39de1a4246f749bacef1271268b429a";
+
+
+
         RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
                 try {
-                    JSONObject object = response.getJSONObject("main");
-                    String temper = object.getString("temp");
+
+                    JSONObject main_object = response.getJSONObject("main");
+                    JSONArray array= response.getJSONArray("weather");
+                     JSONObject object =array.getJSONObject(0);
+
+                    temper = main_object.getString("temp");
                     Double temp = Double.parseDouble(temper) - 273.15;
+                    String WeatherType= object.getString("main");
+                    String humid = main_object.getString("humidity");
+                    String cityn = response.getString("name");
+
+              // Send Data from One activity to another
                     Intent i=new Intent(MainActivity.this,showTemperature.class);
-                    i.putExtra("temperature",temp.toString().substring(0,4));
+
+                    i.putExtra(EXTRA_TEXT,WeatherType);
+                    i.putExtra(EXTRA_NUMBER,temp.toString().substring(0,4));
+                    i.putExtra(EXTRA_NUMBER1,humid.toString());
+                    i.putExtra(EXTRA_TEXT1,cityn);
                     startActivity(i);
-                   // tem.setText(temp.toString().substring(0, 5));
-                    //tem.setText(temperature);
+
                 } catch (JSONException e) {
                     e.printStackTrace();
                     {
@@ -62,8 +100,25 @@ public class MainActivity extends AppCompatActivity {
             }
         }, new Response.ErrorListener() {
             @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(MainActivity.this, error.toString(), Toast.LENGTH_LONG);
+            public void onErrorResponse(VolleyError error) { NetworkResponse response = error.networkResponse;
+                if (response != null && response.statusCode == 404) {
+                    try {
+                        String res = new String(response.data,
+                                HttpHeaderParser.parseCharset(response.headers, "utf-8"));
+                        // Now you can use any deserializer to make sense of data
+                        JSONObject obj = new JSONObject(res);
+                        //use this json as you want
+                    } catch (UnsupportedEncodingException e1) {
+                        // Couldn't properly decode data to string
+                        e1.printStackTrace();
+                    } catch (JSONException e2) {
+                        // returned data is not JSONObject?
+                        e2.printStackTrace();
+                    }
+                }
+
+
+               // Toast.makeText(MainActivity.this, error.toString(), Toast.LENGTH_LONG);
             }
         });
         queue.add(request);
--- Weather/app/src/main/java/com/example/weather/showTemperature.java
+++ Weather/app/src/main/java/com/example/weather/showTemperature.java
@@ -1,23 +1,55 @@
 package com.example.weather;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.icu.util.Calendar;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
+
 public class showTemperature extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_temperature);
 
-        TextView get=(TextView)findViewById(R.id.tv1);
+        ImageView image =(ImageView)findViewById(R.id.img);
+        TextView cn1=(TextView)findViewById(R.id.tv1);
+        TextView temper=(TextView)findViewById(R.id.tv2);
+        TextView desc=(TextView)findViewById(R.id.tv3);
+        TextView hum=(TextView)findViewById(R.id.tv4);
+        TextView date=(TextView)findViewById(R.id.dt);
 
-        Bundle bundle=getIntent().getExtras();
-        String Temper= bundle.getString("temperature");
+        // Receive data from main activity
+        Intent intent=getIntent();
+        String temp =intent.getStringExtra(MainActivity.EXTRA_NUMBER);
+        String humid =intent.getStringExtra(MainActivity.EXTRA_NUMBER1);
+        String  WeatherType=intent.getStringExtra(MainActivity.EXTRA_TEXT);
+        String cityn =intent.getStringExtra(MainActivity.EXTRA_TEXT1);
+
+        Calendar calendar= Calendar.getInstance();
+        SimpleDateFormat sdf=new SimpleDateFormat("dd-MM-yyyy  hh:mm");
+        String dateTime=sdf.format(calendar.getTime());
+
+        //Display data on screen
+
+            image.setImageResource(R.drawable.img1);
+             temper.setText("Temperature  "+temp+ " °C");
+            hum.setText("Humidity  "+humid);
+            desc.setText("Weather  "+WeatherType);
+           cn1.setText(cityn);
+           date.setText("Updated "+dateTime);
 
 
-        get.setText("Temperature is: "+Temper);
+
     }
-}
+
+
+    }
